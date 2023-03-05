747d1c241cfdd252a68970c4211b176cfd850d14
BogdanShevchenko <bogdan.shevchenko42@gmail.com>
2023-03-05 20:34:17 +0200

    small changes

--- clusterisation.py
+++ clusterisation.py
@@ -1,12 +1,15 @@
-from scipy.sparse import dok_matrix, lil_matrix
+import scipy.sparse
+from scipy.sparse import dok_matrix, lil_matrix, spmatrix
 import pandas as pd
 from itertools import permutations
+from typing import Optional
 
 from support_functions import timing
 
 
 @timing(printed_args=['n'])
-def make_sparce_category_matrix(df: pd.DataFrame, n: int, ids_col: str = 'index', max_val: int = None) -> lil_matrix:
+def make_sparce_category_matrix(df: pd.DataFrame, n: int, ids_col: str = 'index',
+                                max_val: Optional[int] = None) -> lil_matrix:
     """
     Convert DataFrame with categories and ids to scipy sparce matrix.
     :param df: DataFrame with column with categories (or infracategories) and column with list of ids, which belong to
@@ -17,6 +20,7 @@ def make_sparce_category_matrix(df: pd.DataFrame, n: int, ids_col: str = 'index'
     :return: sparce matrix n X n where value in cell (i, j) is amount of common categories of article i and article j
     """
     df['len_'] = df[ids_col].apply(len)
+    print('Added', df.eval('len_* (len_ - 1) / 2').sum(), 'new edges')
     category_matrix = dok_matrix((n, n), dtype=int)
     df[ids_col] = df[ids_col].apply(sorted).apply(tuple)
     d = {i: 1 for i in df.loc[df.len_ == 2, ids_col].values}
--- support_functions.py
+++ support_functions.py
@@ -121,33 +121,36 @@ def get_args_dict(f: Callable, args: Sequence, kwargs: dict) -> dict:
     By given function and passed args list and kwargs dict, return dictionary with names and values of all arguments,
     passed to the function, including defaults for those which wasn't passed
     :param f: any function
-    :param args: all positional arguments, passed to function
-    :param kwargs: all named arguments passed to function
-    :return: dictionary with all parameters of the function
+    :param args: all positional arguments, passed to function f
+    :param kwargs: all named arguments passed to function f
+    :return: dictionary with all parameters of the function. If function have **kwargs unpacking, there will be 'kwargs'
+    parameter in resulting dict with all unpacked arguments,
+    and also all this arguments will be in resulting dict by themselves
     """
-    varnames = f.__code__.co_varnames
+    var_names = f.__code__.co_varnames
     pos_arg_count = min(f.__code__.co_argcount, len(args))
     flags = '{:04b}'.format(f.__code__.co_flags)
     has_args = int(flags[-3])
     has_kwargs = int(flags[-4])
-    kwarg_name = [None, varnames[-1]][has_kwargs]
-    arg_name = [None, varnames[-1 - has_kwargs]][has_args]
-    kwargs.update(zip(varnames[:pos_arg_count], args[:pos_arg_count]))
+    kwarg_name = [None, var_names[-1]][has_kwargs]
+    arg_name = [None, var_names[-1 - has_kwargs]][has_args]
+    kwargs.update(zip(var_names[:pos_arg_count], args[:pos_arg_count]))
+    if has_kwargs:
+        kwargs[kwarg_name] = {i: kwargs[i] for i in kwargs if (i not in var_names) or (i in [arg_name, kwarg_name])}
+
     if has_args:
         kwargs[arg_name] = args[pos_arg_count:]
-        defaults = f.__kwdefaults__
+        defaults = f.__kwdefaults__ or {}
     else:
-        args_from_named = varnames[pos_arg_count: len(args)]
+        args_from_named = var_names[pos_arg_count: len(args)]
         args_from_named_values = args[pos_arg_count:]
         kwargs.update(zip(args_from_named, args_from_named_values))
 
-        defaults = f.__defaults__
+        defaults = f.__defaults__ or []
         defaults = dict(zip(
-            varnames[-len(defaults) - has_kwargs:(-1 if has_kwargs else None)],
-            f.__defaults__
+            var_names[-len(defaults) - has_kwargs:(-1 if has_kwargs else None)],
+            defaults
         ))
-    if has_kwargs:
-        kwargs[kwarg_name] = {i: kwargs[i] for i in kwargs if i not in varnames}
     defaults.update(kwargs)
     return defaults
 
